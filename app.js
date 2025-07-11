c1d4331675eff3301cbf8ca41a9b4a378b1a6227
xandyxdev <xandyaragao19@gmail.com>
2025-07-11 08:35:09 -0300

    Projeto iniciado

---
+++ app.js
@@ -0,0 +1,64 @@
+let listaDeNumerosSoreados = [];
+let numeroLimite = 10
+let numeroSecreto =  gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto by xandyx');
+    exibirTextoNaTela('p', `Escolha um número de 1 a ${numeroLimite}`);
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativas}`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++
+        limparCampo();
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementos = listaDeNumerosSoreados.length;
+    if (quantidadeDeElementos == numeroLimite) {
+        listaDeNumerosSoreados = [];
+    }
+    if (listaDeNumerosSoreados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSoreados.push(numeroEscolhido);
+        console.log(listaDeNumerosSoreados)
+        return numeroEscolhido
+    }
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true)
+}
